2609556687a6873788ce5a00731e68e4f5859fee
rakshajha-tab <rakshaj337@gmail.com>
2026-09-15 12:24:35 +0530

    Time: 2 ms (72.78%) | Memory: 14.7 MB (58.68%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,13 +1,48 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for(int i = 0; i < nums.size(); i++) {
-            for(int j = i + 1; j < nums.size(); j++) {
-                if(nums[i] + nums[j] == target) {
-                    return {i, j};
+        vector<int> ans;
+
+        vector<int> sorted = nums;
+        sort(sorted.begin(), sorted.end());
+
+        int n = sorted.size();
+        int i = 0;
+        int j = n - 1;
+
+        while(i < j) {
+            int pairSum = sorted[i] + sorted[j];
+
+            if(pairSum > target) {
+                j--;
+            }
+            else if(pairSum < target) {
+                i++;
+            }
+            else {
+                int left = sorted[i];
+                int right = sorted[j];
+
+                // first number ka original index
+                for(i = 0; i < n; i++) {
+                    if(nums[i] == left) {
+                        ans.push_back(i);
+                        break;
+                    }
                 }
+
+                // second number ka original index
+                for(j = 0; j < n; j++) {
+                    if(nums[j] == right && j != ans[0]) {
+                        ans.push_back(j);
+                        break;
+                    }
+                }
+
+                return ans;
             }
         }
-        return {} ;
+
+        return ans;
     }
 };
